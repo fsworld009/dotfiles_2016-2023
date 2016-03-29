011602b43e1afd4daa21194e50eeac2ca4461cb9
WorldFS <fsworld009@gmail.com>
2016-03-29 01:05:44 -0400

    .editconfig

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ cd ~
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 exit
