51f5baf8d3b51b425d02ba78c517ab5f556f2730
WorldFS <fsworld009@gmail.com>
2016-07-03 22:26:07 -0400

    add ctags

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.editorconfig ~/.editorconfig
+ln -s ~/dotfiles/.ctags ~/.ctags
 exit
