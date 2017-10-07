e0bd3229b22f53ac2e82ac31a241a0d6b30522c6
WorldFS <fsworld009@gmail.com>
2017-10-07 10:42:34 +0800

    change IME to ficix

--- x11forward_setup.sh
+++ x11forward_setup.sh
@@ -1,4 +1,4 @@
-export XMODIFIERS=@im=gcin
-export GTK_IM_MODULE="gcin"
-export QT_IM_MODULE="gcin"
-gcin &
+export XMODIFIERS="@im=fcitx"
+export GTK_IM_MODULE="fcitx"
+export QT_IM_MODULE="fcitx"
+fcitx &
