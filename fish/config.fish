53931ccc07623b19c05be8a5d7c39ad88dd12159
WorldFS <fsworld009@gmail.com>
2022-11-24 17:15:23 +0800

    config.fish: fix grep command

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
-if begin type -q pip; and test (pip list | grep virtualfish ^ /dev/null); end
+if begin type -q pip; and test (pip list | grep virtualfish); end
   eval (python -m virtualfish compat_aliases)
 end
 
 # http://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
 function fish_prompt
-    set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+    set -l git_branch (git branch | sed -n '/\* /s///p')
     set_color green 
     echo -n (prompt_pwd)
     set_color purple
